095ad7d93087270948f19ec7cd765b2db1f9feb5
Rofilde Hasudungan <rofilde-umkt@umkt.ac.id>
2019-10-24 17:15:04 +0800

    link halaman depan dengan data di database

--- app/Barang.php
+++ app/Barang.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     protected $table = "barang";
-	protected $fillable = ['nama','katagori_id','harga','toko_id','foto', 'stok'];
+    protected $fillable = ['nama','katagori_id','harga','toko_id','foto', 'stok'];
+    
+    public function toko()
+    {
+        return $this->belongsTo(Toko::class, "toko_id", "id");
+    }
 }
