37f9b3ab95420cec86e374b8c4551af8c21fa817
Beta Project <beta.project.iwz@gmail.com>
2019-10-24 19:29:54 +0800

    perubahan

--- resources/views/makananringan.blade.php
+++ resources/views/makananringan.blade.php
@@ -31,7 +31,7 @@
             <!-- Logo Header -->
             <div class="logo-header" data-background-color="blue">
                 
-                <a href="index.html" class="logo">
+                <a href="http://127.0.0.1:8000/" class="logo">
                     <img src="../assets/img/logo.svg" alt="navbar brand" class="navbar-brand">
                 </a>
                 <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
