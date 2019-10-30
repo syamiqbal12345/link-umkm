947b1d6f39564e291ffc4ccb26512b99d837bc17
Rofilde Hasudungan <rofilde-umkt@umkt.ac.id>
2019-10-30 12:20:38 +0800

    front page

--- resources/views/transaksi/index.blade.php
+++ resources/views/transaksi/index.blade.php
@@ -11,6 +11,7 @@
 		</div>
 
 		<div class="card-body">
+            {{ $transaksi }}
 		<table class="table table-bordered">
 		<thead>
 			<tr>
@@ -23,25 +24,25 @@
 				<th>Rating</th>
 			</tr>
 		</thead>
-<tbody>
-@foreach ($transaksi as $no=>$item)
-	<tr>
-		<td>{{ $no + 1 }}</td>
-		<td>{{ $item->tanggal }} </td>
-		<td>{{ $item->status }} </td>
-		<td>{{ $item->total }} </td>
-		<td>{{ $item->alamat }}</td>
-		<td>{{ $item->biaya_kurir }}</td>
-		<td>{{ $item->rating }}</td>
-		<td><img src="{{ $item->foto }}" width="100"> </td>
-		<td>
-		<div class="btn-group btn-group-sm">
-			<a class="btn btn-warning" href="{{ route('transaksi.edit', ['id' => $item->id]) }}">Edit</a>
-			<a class="btn btn-danger" href="{{ route('transaksi.delete', ['id' => $item->id]) }}">Del</a>
-						</div>
-					</td>
-				</tr>
-			@endforeach
+        <tbody>
+        @foreach ($transaksi as $no=>$item)
+            <tr>
+                <td>{{ $no + 1 }}</td>
+                <td>{{ $item->tanggal }} </td>
+                <td>{{ $item->status }} </td>
+                <td>{{ $item->total }} </td>
+                <td>{{ $item->alamat }}</td>
+                <td>{{ $item->biaya_kurir }}</td>
+                <td>{{ $item->rating }}</td>
+                <td><img src="{{ $item->foto }}" width="100"> </td>
+                <td>
+                    <div class="btn-group btn-group-sm">
+                        <a class="btn btn-warning" href="{{ route('transaksi.edit', ['id' => $item->id]) }}">Edit</a>
+                        <a class="btn btn-danger" href="{{ route('transaksi.delete', ['id' => $item->id]) }}">Del</a>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
 		</tbody>
 	</table>
 		</div>
