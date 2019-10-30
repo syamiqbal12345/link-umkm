437e805fc2b977599a442a03e4db2aaa2f0a2878
Rofilde Hasudungan <rofilde-umkt@umkt.ac.id>
2019-10-30 12:36:19 +0800

    tampil transaksi

--- resources/views/transaksi/index.blade.php
+++ resources/views/transaksi/index.blade.php
@@ -15,12 +15,14 @@
 		<thead>
 			<tr>
 				<th>#</th>
+
 				<th>Tanggal</th>
-				<th>Status</th>
-				<th>Total</th>
-				<th>Alamat</th>
-				<th>Biaya_Kurir</th>
-				<th>Rating</th>
+                <th>Alamat</th>
+				<th>Belanja</th>
+				<th>Biaya Kurir</th>
+				<th>TOTAL</th>
+                <th>Status</th>
+                <th><i class="fa fa-eye"></i></th>
 			</tr>
 		</thead>
         <tbody>
@@ -28,14 +30,13 @@
             <tr>
                 <td>{{ $no + 1 }}</td>
                 <td>{{ $item->tanggal }} </td>
-                <td>{{ $item->status }} </td>
-                <td>{{ $item->total }} </td>
                 <td>{{ $item->alamat }}</td>
+                <td>{{ $item->total - $item->biaya_kurir }} </td>
                 <td>{{ $item->biaya_kurir }}</td>
-                <td>{{ $item->rating }}</td>
-                <td><img src="{{ $item->foto }}" width="100"> </td>
+                <td>{{ $item->total }} </td>
+                <td>{{ $item->status }} </td>
                 <td>
-                    <a class="btn btn-warning" href="{{ route('transaksi.show', ['id' => $item->id]) }}"><i class="fa fa-show"></i></a>
+                    <a class="btn btn-warning" href="{{ route('transaksi.show', ['id' => $item->id]) }}"><i class="fa fa-search"></i></a>
 
                 </td>
             </tr>
