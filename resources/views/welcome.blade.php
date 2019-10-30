7eb6394d27d11ea902ffe21f6db07fdf4c8b30ca
syamiqbal12345 <syamiqbal12345@gmai.com>
2019-10-29 23:24:04 -0700

    penambahan about

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -246,28 +246,11 @@
                                 <span>
                                     M Iqbal
                                     <span class="user-level">User</span>
-                                    <span class="caret"></span>
-                                </span>
+                                    </span>
                             </a>
                             <div class="clearfix"></div>
-
                             <div class="collapse in" id="collapseExample">
                                 <ul class="nav">
-                                    <li>
-                                        <a href="#profile">
-                                            <span class="link-collapse">My Profile</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#edit">
-                                            <span class="link-collapse">Edit Profile</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#settings">
-                                            <span class="link-collapse">Settings</span>
-                                        </a>
-                                    </li>
                                 </ul>
                             </div>
                         </div>
